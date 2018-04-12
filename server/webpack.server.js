514b1020220b81e8d8afd78aea7fecf4ef9eab6c
US\amidash <amidash@deloitte.com>
2018-04-12 18:14:32 +0530

    Added .gitignore

---
+++ server/webpack.server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+    //Inform webpack to build a bundle for NodeJS and not the browser
+    target: 'node',
+    //The root file of our server application(entry point)
+    entry: './src/index.js',
+    //Tell webpack where to put the bundled output file
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    // Tell webpack to run babel on every file it runs through
+    module:{
+        rules: [{
+            test: /\.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: {
+                presets: [
+                    'react', 
+                    'stage-0',
+                    ['env', { targets: { browsers: ['last 2 versions'] } } ]
+                ]
+            }
+        }]
+    }
+};
