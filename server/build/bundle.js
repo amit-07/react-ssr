8c9b534b8d932a02b93476743bd11eb1ad82a7f9
US\amidash <amidash@deloitte.com>
2018-04-13 12:13:03 +0530

    Changes to intoduce client side  rendering.

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -6969,16 +6969,29 @@ module.exports = memoizeStringOnly;
 "use strict";
 
 
-var express = __webpack_require__(52);
-var React = __webpack_require__(18);
-var renderToString = __webpack_require__(108).renderToString;
-var Home = __webpack_require__(117).default;
-var app = express();
+var _express = __webpack_require__(52);
 
+var _express2 = _interopRequireDefault(_express);
+
+var _react = __webpack_require__(18);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(108);
+
+var _home = __webpack_require__(117);
+
+var _home2 = _interopRequireDefault(_home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.use(_express2.default.static('public'));
 app.get('/', function (req, res) {
-    var content = renderToString(React.createElement(Home, null));
-
-    res.send(content);
+    var content = (0, _server.renderToString)(_react2.default.createElement(_home2.default, null));
+    var html = '\n        <html>\n            <head>\n            </head>\n            <body>\n                <div id="root">' + content + '</div>\n                <script src="bundle.js"></script>\n            </body>\n        </html>\n    ';
+    res.send(html);
 });
 
 app.listen(3000, function () {
@@ -22082,7 +22095,18 @@ var Home = function Home() {
     return _react2.default.createElement(
         'div',
         null,
-        'I\'m the home component'
+        _react2.default.createElement(
+            'div',
+            null,
+            'I\'m the home component'
+        ),
+        _react2.default.createElement(
+            'button',
+            { onClick: function onClick() {
+                    return console.log('Hi there!');
+                } },
+            'Press here!'
+        )
     );
 };
 
---
+++ server/src/client/client.js
@@ -0,0 +1,6 @@
+// Start up point for client side application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/home';
+
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
